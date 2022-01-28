8c2c89b39b725c0d2561ff5802ebcc09580e1092
Erin Han <eshan6597@gmail.com>
2022-01-27 19:46:13 -0700

    Anagrams

--- AlgoCasts/exercises/anagrams/index.js
+++ AlgoCasts/exercises/anagrams/index.js
@@ -8,6 +8,95 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+const clean = (str) => {
+  return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
+};
+
+function anagrams(stringA, stringB) {
+  return clean(stringA) === clean(stringB);
+}
 
 module.exports = anagrams;
+
+/**
+ * 
+ * NOTES
+ * 
+ * HELPER FUNCTIONS - help organize and minimize code;  
+OLDER ATTEMPTS
+
+3. CLEANED UP
+const clean = (str) => {
+  return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
+};
+
+function anagrams(stringA, stringB) {
+  return clean(stringA) === clean(stringB);
+}
+
+2. WORKING ATTEMPT W ARRAYS
+  const cleanA = stringA.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
+  const cleanB = stringB.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
+
+  return cleanA === cleanB;
+
+1. EXAMPLE WITH NOTES
+const clean = (str) => {
+  //Clean anything that's not a letter and ignore capitalization
+  return str.replace(/[^\w]/g, '').toLowerCase();
+};
+
+const charMap = (str) => {
+  //Create map object with quantities of all characters
+  const map = {};
+
+  //Count characters
+  for (let char of str) {
+    map[char] = map[char] + 1 || 1;
+  }
+
+  return map;
+}
+
+function anagrams(stringA, stringB) {
+  const mapA = charMap(stringA);
+  const mapB = charMap(stringB);
+
+  //Check both contain same # of types
+  if (Object.keys(mapA).length !== Object.keys(mapB).length) {
+    return false;
+  }
+
+  //Loop through A to check quantities
+  for (let char in mapA) {
+    if (mapA[char] !== mapB[char]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+1. BLIND
+  //Assign to array
+  const formatA = stringA.toLowerCase()
+    //replace spaces
+    .replace(/\s+/g, '')
+    //remove punct
+    .replace(/[-!?.,]/g, '')
+    //convert to array
+    .split('')
+    //sort ascending
+    .sort()
+    //convert back to string
+    .join('');
+
+  const formatB = stringB.toLowerCase()
+    .replace(/\s+/g, '')
+    .replace(/[-!?.,]/g, '')
+    .split('')
+    .sort()
+    .join();
+  
+  return formatA === formatB;
+ */
