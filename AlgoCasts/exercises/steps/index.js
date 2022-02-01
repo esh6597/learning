77069889326bda9bbb9abb9e2fc74b0f8421f712
Erin Han <eshan6597@gmail.com>
2022-02-01 14:49:32 -0700

    Steps part 2

--- AlgoCasts/exercises/steps/index.js
+++ AlgoCasts/exercises/steps/index.js
@@ -17,23 +17,46 @@
 //       '### '
 //       '####'
 
-function steps(n) {
-  for (let row = 0; row < n; row++) {
-    let stair = '';
+function steps(n, row = 0, stair = '') {
+  if (n === row) {
+    return;
+  }
 
-    for (let column = 0; column < n; column++) {
-      if (column <= row) {
-        stair += '#';
-      } else {
-        stair += ' ';
-      }
-    }
-
+  if (n === stair.length) {
     console.log(stair);
-  }  
+    return steps(n, row + 1);
+  }
+
+  if (stair.length <= row) {
+    stair += '#';
+  } else {
+    stair += ' ';
+  }
+
+  steps(n, row, stair);
 }
 
 module.exports = steps;
+
+// 3. RECURSIVE ANSWER
+// function steps(n, row = 0, stair = '') {
+//   if (n === row) {
+//     return;
+//   }
+
+//   if (n === stair.length) {
+//     console.log(stair);
+//     return steps(n, row + 1);
+//   }
+
+//   if (stair.length <= row) {
+//     stair += '#';
+//   } else {
+//     stair += ' ';
+//   }
+
+//   steps(n, row, stair);
+// }
 
 // 2. ANSWER 1
 // for (let row = 0; row < n; row++) {
