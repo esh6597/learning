6417761705a4a5eac664bdde7563015107a6bcc0
Erin Han <eshan6597@gmail.com>
2022-02-04 14:08:28 -0700

    Matrix Solutions

--- AlgoCasts/exercises/matrix/index.js
+++ AlgoCasts/exercises/matrix/index.js
@@ -15,6 +15,124 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+ 
+}
 
 module.exports = matrix;
+
+// 2. GUIDED ATTEMPT AT DIFFERENT METHOD (NOT WORKING)
+// const matrix = [];
+
+// //Create and push row arrays
+// for (let r = 0; r < n; r++) {
+//   let rowArr = [];
+//   for (let c = 0; c < n; c++) {
+//     //Add searchable unique decimal to create columns
+//     rowArr.push(0.5);
+//   }
+//   //Push to matrix
+//   matrix.push(rowArr);
+// }
+
+// //Initialize coordinates and start at 1st square
+// let row = 0;
+// let col = 0;
+// let direction = 'right';
+// //Loop and place spiral numbers
+// for (let counter = 1; counter <= n^2; counter++) {
+//   if (matrix[row][col] === 0.5) {
+//     matrix[row][col] = counter;
+//   }
+  
+//   //Going right
+//   if (direction === 'right') {
+//     //Hit corner going right OR next coordinate isn't placeholder
+//     if (col + 1 === n || matrix[row][col + 1] !== 0.5) {
+//       direction = 'down';
+//       row++;
+//     } else {
+//       col++;
+//     }
+//   }
+
+//   //Down
+//   if (direction === 'down') {
+//     if (row + 1 === n || matrix[row + 1][col] !== 0.5) {
+//       direction = 'left';
+//       col--;
+//     } else {
+//       row++;
+//     }
+//   }
+
+//   //Left
+//   if (direction === 'left') {
+//     if (col - 1 === -1 || matrix[row][col - 1] !== 0.5) {
+//       direction = 'up';
+//       row--;
+//     } else {
+//       col--;
+//     }
+//   }
+
+//   //Up
+//   if (direction === 'up') {
+//     if (row - 1 === -1 || matrix[row - 1][col] !== 0.5) {
+//       direction = 'right';
+//       col++;
+//     } else {
+//       row--;
+//     }
+//   }
+// }
+
+// return matrix;
+
+// 1. ANSWER 1 VIA ROW AND COL MOVING
+//  const results = [];
+
+//   for (let r = 0; r < n; r++) {
+//     results.push([]);
+//   }
+
+//   let counter = 1;
+//   let startColumn = 0;
+//   let endColumn = n - 1;
+//   let startRow = 0;
+//   let endRow = n - 1;
+
+//   while (startColumn <= endColumn && startRow <= endRow) {
+//     // Top row
+//     for (let i = startColumn; i <= endColumn; i++) {
+//       results[startRow][i] = counter;
+//       counter++;
+//     }
+//     startRow++;
+
+//     //Right column
+//     for (let i = startRow; i <= endRow; i++) {
+//       results[i][endColumn] = counter;
+//       counter++;
+//     }
+//     endColumn--;
+
+//     //Bottom row
+//     for (let i = endColumn; i >= startColumn; i--) {
+//       results[endRow][i] = counter;
+//       counter++
+//     }
+//     endRow--;
+
+//     // Start column
+//     for (let i = endRow; i>= startRow; i--) {
+//       results[i][startColumn] = counter;
+//       counter++;
+//     }
+//     startColumn++;
+//   }
+
+//   return results;
+
+// IN PROGRESS ATTEMPT
+
