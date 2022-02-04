e270c47429e4fcc3c7dccdaafbd3d3feff027746
Erin Han <eshan6597@gmail.com>
2022-02-04 12:07:35 -0700

    Pyramids part 2

--- AlgoCasts/exercises/pyramid/index.js
+++ AlgoCasts/exercises/pyramid/index.js
@@ -15,22 +15,57 @@
 //       '#####'
 
 function pyramid(n) {
-  //loop 1 to n ROWS
-  for (let row = 1; row <= n; row++) {
-    let print = [];
-    for (let b = 1; b <= (2 * (n - row)); b++) {
-      print.push(' ');
-    }
-    for (let c = 1; c <= ((2 * row) - 1); b++) {
-      print.splice(n - row, '#');
-    }
+  const midpoint = Math.floor((2*n - 1) / 2);
 
-    console.log(print.join(''));
+  for (let row = 0; row < n; row++) {
+    let level = '';
+
+    for (let column = 0; column < 2*n - 1; column++) {
+      if (midpoint - row <= column && midpoint + row >= column) {
+        level += '#';
+      } else {
+        level += ' ';
+      }
+    }
   }
-  //for each row:
-  //  add 2(n - ROW) spaces
-  //  in middle, add (2 * ROW) - 1 hashes
-  //  print
+
+  console.log(level);
 }
 
 module.exports = pyramid;
+
+// 2. RECURSIVE SOLUTION
+// function pyramid(n, row = 0, level = '') {
+//   if (row === n) {
+//     return;
+//   }
+
+//   if (level.length === 2*n - 1) {
+//     console.log(level);
+//     return pyramid(n, row + 1);
+//   }
+
+//   const midpoint = Math.floor((2*n - 1) / 2);
+//   let add;
+
+//   if (midpoint - row <= level.length && midpoint + row >= level.length) {
+//     add = '#';
+//   } else {
+//     add = ' ';
+//   }
+//   pyramid(n, row, level + add);
+// }
+
+
+// 1. WORKING BLIND ITERATIVE ATTEMPT
+// for (let row = 1; row <= n; row++) {
+//   let print = [];
+//   for (let b = 1; b <= (2 * (n - row)); b++) {
+//     print.push(' ');
+//   }
+//   for (let c = 1; c <= ((2 * row) - 1); c++) {
+//     print.splice(n - row, 0, '#');
+//   }
+
+//   console.log(print.join(''));
+// }
