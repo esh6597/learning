5921e74d81a4dc95f898704b0028562a42d79346
Erin Han <eshan6597@gmail.com>
2022-02-18 14:54:54 -0700

    Fibonacci blind attempts

--- AlgoCasts/exercises/fib/index.js
+++ AlgoCasts/exercises/fib/index.js
@@ -8,6 +8,42 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function fib(n) {
+
+}
 
 module.exports = fib;
+
+// 2. BLIND RECURSIVE ATTEMPT
+// function fib(n, step = 1, sum = 1, last = 0) {
+//   if (n === 0 || n < 0) {
+//     return 0;
+//   } else if (n === step) {
+//     return sum;
+//   } else {
+//     let mem = sum + last;
+//     last = sum;
+//     sum = mem;
+//     step++;
+//     fib(n, step, sum, last);
+//   }
+// }
+
+
+// 1. BLIND WORKING ATTEMPT
+// //Use 2 element array for memory
+// const mem = [0, 1];
+// if (n === 0) {
+//   return 0;
+// } else if (n === 1 || n === 2) {
+//   return 1;
+// }
+// //loop to n
+// //add 1 elements together + assign
+// //move mem1 to mem0 then assign add to mem1
+// for (let i = 0; i < n - 1; i++) {
+//   let sum = mem[0] + mem[1];
+//   mem[0] = mem[1];
+//   mem[1] = sum;
+// }
+// return mem[1];
