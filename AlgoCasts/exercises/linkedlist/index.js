ceeefaa745ce89b7a89b3c5a315b52d75ea8f41f
Erin Han <eshan6597@gmail.com>
2022-03-05 22:21:09 -0500

    Linked lists: setup and notes

--- AlgoCasts/exercises/linkedlist/index.js
+++ AlgoCasts/exercises/linkedlist/index.js
@@ -2,6 +2,18 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
+// THIS IS A VERY COMMON INTERVIEW CONCEPT.
+/**
+ * - A chain of nodes where each node is linked to the next
+ * - order is always maintained (unless we want to)
+ * - HEAD: 1st node
+ * - TAIL: last node: can be ID'd by its lack of link
+ * - NODE: unit
+ * - DATA + REFERENCE TO NEXT NODE: makes up a node
+ * - Data can be an array or anything
+ * - REFERENCE: node1 {} can be linked via: node1.next = node2;
+ */
+
 class Node {}
 
 class LinkedList {}
